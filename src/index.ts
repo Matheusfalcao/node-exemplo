3545fba6529b3c373da6b372c985a681c0c20852
MATHEUS NAVARRO FERREIRA RIOS FALCAO <matheus.nfrfalcao@senacsp.edu.br>
2023-11-08 14:56:41 -0300

    configuracao do ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import validator from "validator"
+
+console.log(validator.isEmail('teste@gmail.com'))
+
+let nome: String = "João"
+let idade: Number = 18
+
+console.log(`meu nome é ${nome} e eu tenho ${idade} anos`)
+
+
